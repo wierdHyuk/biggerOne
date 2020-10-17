dd8ebd25619fbe6e830e560e8813df2026eb2a40
SeungHyunHong <noa316@naver.com>
2020-10-17 22:13:19 +0900

    달력, 지도표시 기능구현 (#7)

---
+++ src/Main/Start.java
@@ -0,0 +1,15 @@
+package Main;
+
+import Calendar.MapCalendar;
+
+import javax.swing.*;
+
+public class Start {
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(new Runnable(){
+            public void run(){
+                new MapCalendar();
+            }
+        });
+    }
+}
---
+++ src/Map/imagePanel.java
@@ -0,0 +1,26 @@
+package Map;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class imagePanel extends JPanel {
+    Image image = null;
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        ImageIcon map = new ImageIcon( Toolkit.getDefaultToolkit().getImage(getClass().getResource("map.png")));
+        image = map.getImage();
+
+        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        int w = getWidth();
+        int h = getHeight();
+        Color color1 = new Color(0xF2BE65);
+        Color color2 = Color.white;
+        GradientPaint gp = new GradientPaint(10, 10, color1, 0, h, color1);
+
+        g2d.setPaint(gp);
+        g2d.fillRect(0, 0, w, h);
+        g.drawImage(image, 0, 0, 840, 550, this);
+    }
+}
