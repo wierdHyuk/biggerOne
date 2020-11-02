5c3a0510bffa29d9eca2e139eb1dc54c7eefae44
wierdHyuk <test1for2proj3@gmail.com>
2020-11-02 17:53:26 +0900

    throw 사용

--- src/com/covid/Start.java
+++ src/com/covid/Start.java
@@ -1,11 +1,14 @@
 package com.covid;
 
 import com.covid.Front.Calendar.MapCalendar;
+import com.covid.Model.PatientModel;
+import com.covid.Util.DataHandler.ReadCSV;
 
 import javax.swing.*;
+import java.util.List;
 
 public class Start {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
         SwingUtilities.invokeLater(new Runnable(){
             public void run(){
                 new MapCalendar();
