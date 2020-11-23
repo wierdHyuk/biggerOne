193050e17a9598f0365fed59efd070daa4e01417
wierdHyuk <test1for2proj3@gmail.com>
2020-11-24 01:39:27 +0900

    전역 변수로 counts 선언

--- src/main/java/com/covid/Model/Const.java
+++ src/main/java/com/covid/Model/Const.java
@@ -1,5 +1,12 @@
 package com.covid.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Const {
     public static String url = "jdbc:mysql://3.34.52.183/covid19?&useSSL=false&characterEncoding=UTF-8&serverTimezone=UTC";
+   
+
+    public static List<Long> counts = null;
 }
